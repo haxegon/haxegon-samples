228f27aae4102f7bf410ddc795d198ffb6feeb11
TerryCavanagh <terrygcavanagh@gmail.com>
2016-11-18 14:59:16 +0000

    Some basic examples to get started

---
+++ simple/01 - hello world/src/Main.hx
@@ -0,0 +1,7 @@
+import haxegon.*;
+
+class Main {
+	function update() {
+		Text.display(0, 0, "Hello World");
+	}
+}
---
+++ simple/02 - bitmap fonts/bin/flash/debug/haxe/ApplicationMain.hx
@@ -0,0 +1,538 @@
+package;
+
+
+#if macro
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+#end
+
+@:access(lime.app.Application)
+@:access(lime.Assets)
+@:access(openfl.display.Stage)
+
+
+class ApplicationMain {
+	
+	
+	#if !macro
+	
+	
+	public static var config:lime.app.Config;
+	public static var preloader:openfl.display.Preloader;
+	
+	
+	public static function main () {
+		
+		config = {
+			
+			build: "1",
+			company: "Your name here",
+			file: "bitmapfonts",
+			fps: 60,
+			name: "Bitmap Fonts",
+			orientation: "landscape",
+			packageName: "com.haxegon.bitmapfonts",
+			version: "1.0.0",
+			windows: [
+				
+				{
+					allowHighDPI: true,
+					antialiasing: 0,
+					background: 0,
+					borderless: false,
+					depthBuffer: false,
+					display: 0,
+					fullscreen: false,
+					hardware: true,
+					height: 480,
+					hidden: #if munit true #else null #end,
+					maximized: null,
+					minimized: null,
+					parameters: "{}",
+					resizable: true,
+					stencilBuffer: true,
+					title: "Bitmap Fonts",
+					vsync: true,
+					width: 768,
+					x: null,
+					y: null
+				},
+			]
+			
+		};
+		
+		#if hxtelemetry
+		var telemetry = new hxtelemetry.HxTelemetry.Config ();
+		telemetry.allocations = true;
+		telemetry.host = "localhost";
+		telemetry.app_name = config.name;
+		Reflect.setField (config, "telemetry", telemetry);
+		#end
+		
+		#if (js && html5)
+		#if (munit || utest)
+		embed (null, 768, 480, "000000");
+		#end
+		#else
+		create ();
+		#end
+		
+	}
+	
+	
+	public static function create ():Void {
+		
+		var app = new openfl.display.Application ();
+		app.create (config);
+		
+		preloader = getPreloader ();
+		preloader.onComplete.add (registerLibrary);
+		app.setPreloader (preloader);
+		preloader.onComplete.add (init);
+		preloader.create (config);
+		
+		#if (js && html5)
+		var urls = [];
+		var types = [];
+		
+		
+		urls.push ("data/graphics/fonts/04b11/04b11.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/04b11/04b11_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/c64/c64.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/c64/c64_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/comic/comic.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/comic/comic_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/crypt/crypt.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/crypt/crypt_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/default/default.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/default/default_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/dos/dos.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/dos/dos_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/ganon/ganon.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/ganon/ganon_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/nokia/nokia.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/nokia/nokia_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/oldenglish/oldenglish.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/oldenglish/oldenglish_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/pixel/pixel.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/pixel/pixel_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/pressstart/pressstart.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/pressstart/pressstart_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/retrofuture/retrofuture.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/retrofuture/retrofuture_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/roman/roman.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/roman/roman_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/special/special.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/special/special_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/thin/thin.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/thin/thin_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/tiny/tiny.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/tiny/tiny_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		urls.push ("data/graphics/fonts/yoster/yoster.fnt");
+		types.push (lime.Assets.AssetType.TEXT);
+		
+		
+		urls.push ("data/graphics/fonts/yoster/yoster_0.png");
+		types.push (lime.Assets.AssetType.IMAGE);
+		
+		
+		
+		if (config.assetsPrefix != null) {
+			
+			for (i in 0...urls.length) {
+				
+				if (types[i] != lime.Assets.AssetType.FONT) {
+					
+					urls[i] = config.assetsPrefix + urls[i];
+					
+				}
+				
+			}
+			
+		}
+		
+		preloader.load (urls, types);
+		#end
+		
+		var result = app.exec ();
+		
+		#if (sys && !ios && !nodejs && !emscripten)
+		lime.system.System.exit (result);
+		#end
+		
+	}
+	
+	
+	#if (js && html5)
+	@:keep @:expose("lime.embed")
+	public static function embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
+		
+		var htmlElement:js.html.Element = null;
+		
+		if (Std.is (element, String)) {
+			
+			htmlElement = cast js.Browser.document.getElementById (cast (element, String));
+			
+		} else if (element == null) {
+			
+			htmlElement = cast js.Browser.document.createElement ("div");
+			
+		} else {
+			
+			htmlElement = cast element;
+			
+		}
+		
+		var color = null;
+		
+		if (background != null && background != "") {
+			
+			background = StringTools.replace (background, "#", "");
+			
+			if (background.indexOf ("0x") > -1) {
+				
+				color = Std.parseInt (background);
+				
+			} else {
+				
+				color = Std.parseInt ("0x" + background);
+				
+			}
+			
+		}
+		
+		if (width == null) {
+			
+			width = 0;
+			
+		}
+		
+		if (height == null) {
+			
+			height = 0;
+			
+		}
+		
+		config.windows[0].background = color;
+		config.windows[0].element = htmlElement;
+		config.windows[0].width = width;
+		config.windows[0].height = height;
+		config.assetsPrefix = assetsPrefix;
+		
+		create ();
+		
+	}
+	
+	
+	@:keep @:expose("openfl.embed")
+	public static function _embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
+		
+		embed (element, width, height, background, assetsPrefix);
+		
+	}
+	#end
+	
+	
+	public static function init ():Void {
+		
+		var loaded = 0;
+		var total = 0;
+		var library_onLoad = function (__) {
+			
+			loaded++;
+			
+			if (loaded == total) {
+				
+				start ();
+				
+			}
+			
+		}
+		
+		preloader = null;
+		
+		
+		
+		
+		if (total == 0) {
+			
+			start ();
+			
+		}
+		
+	}
+	
+	
+	private static function registerLibrary ():Void {
+		
+		lime.Assets.registerLibrary ("default", new DefaultAssetLibrary ());
+		
+	}
+	
+	
+	public static function start ():Void {
+		
+		ApplicationMain.getEntryPoint ();
+		
+		#if !flash
+		openfl.Lib.current.stage.dispatchEvent (new openfl.events.Event (openfl.events.Event.RESIZE, false, false));
+		
+		if (openfl.Lib.current.stage.window.fullscreen) {
+			
+			openfl.Lib.current.stage.dispatchEvent (new openfl.events.FullScreenEvent (openfl.events.FullScreenEvent.FULL_SCREEN, false, false, true, true));
+			
+		}
+		#end
+		
+	}
+	
+	
+	#end
+	
+	
+	macro public static function getEntryPoint () {
+		
+		var hasMain = false;
+		
+		switch (Context.follow (Context.getType ("haxegon.Load"))) {
+			
+			case TInst (t, params):
+				
+				var type = t.get ();
+				
+				for (method in type.statics.get ()) {
+					
+					if (method.name == "main") {
+						
+						hasMain = true;
+						break;
+						
+					}
+					
+				}
+				
+				if (hasMain) {
+					
+					return Context.parse ("@:privateAccess haxegon.Load.main ()", Context.currentPos ());
+					
+				} else if (type.constructor != null) {
+					
+					return macro { new DocumentClass (); };
+					
+				} else {
+					
+					Context.fatalError ("Main class \"haxegon.Load\" has neither a static main nor a constructor.", Context.currentPos ());
+					
+				}
+				
+			default:
+				
+				Context.fatalError ("Main class \"haxegon.Load\" isn't a class.", Context.currentPos ());
+			
+		}
+		
+		return null;
+		
+	}
+	
+	
+	macro public static function getPreloader () {
+		
+		
+		return macro { new openfl.display.Preloader (new openfl.display.Preloader.DefaultPreloader ()); };
+		
+		
+	}
+	
+	
+	#if (neko && !macro)
+	@:noCompletion @:dox(hide) public static function __init__ () {
+		
+		// Copy from https://github.com/HaxeFoundation/haxe/blob/development/std/neko/_std/Sys.hx#L164
+		// since Sys.programPath () isn't available in __init__
+		var sys_program_path = {
+			var m = neko.vm.Module.local().name;
+			try {
+				sys.FileSystem.fullPath(m);
+			} catch (e:Dynamic) {
+				// maybe the neko module name was supplied without .n extension...
+				if (!StringTools.endsWith(m, ".n")) {
+					try {
+						sys.FileSystem.fullPath(m + ".n");
+					} catch (e:Dynamic) {
+						m;
+					}
+				} else {
+					m;
+				}
+			}
+		};
+		
+		var loader = new neko.vm.Loader (untyped $loader);
+		loader.addPath (haxe.io.Path.directory (#if (haxe_ver >= 3.3) sys_program_path #else Sys.executablePath () #end));
+		loader.addPath ("./");
+		loader.addPath ("@executable_path/");
+		
+	}
+	#end
+	
+	
+}
+
+
+#if !macro
+
+
+@:build(DocumentClass.build())
+@:keep class DocumentClass extends haxegon.Load {}
+
+
+#else
+
+
+class DocumentClass {
+	
+	
+	macro public static function build ():Array<Field> {
+		
+		var classType = Context.getLocalClass ().get ();
+		var searchTypes = classType;
+		
+		while (searchTypes != null) {
+			
+			if (searchTypes.pack.length == 2 && searchTypes.pack[1] == "display" && searchTypes.name == "DisplayObject") {
+				
+				var fields = Context.getBuildFields ();
+				
+				var method = macro {
+					
+					openfl.Lib.current.addChild (this);
+					super ();
+					dispatchEvent (new openfl.events.Event (openfl.events.Event.ADDED_TO_STAGE, false, false));
+					
+				}
+				
+				fields.push ({ name: "new", access: [ APublic ], kind: FFun({ args: [], expr: method, params: [], ret: macro :Void }), pos: Context.currentPos () });
+				
+				return fields;
+				
+			}
+			
+			if (searchTypes.superClass != null) {
+				
+				searchTypes = searchTypes.superClass.t.get ();
+				
+			} else {
+				
+				searchTypes = null;
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	
+}
+
+
+#end
