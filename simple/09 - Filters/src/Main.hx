cba161cd5d1199e68bdc18d9dc770d9225a27d2a
TerryCavanagh <terrygcavanagh@gmail.com>
2017-06-02 17:21:34 +0100

    Added Filters example

---
+++ simple/09 - Filters/src/Main.hx
@@ -0,0 +1,39 @@
+import haxegon.*;
+
+class Main {	
+	function new(){
+		Gfx.loadimage("constellation");
+	}
+	
+	var currentfilter = "None";
+	
+	function update(){
+		Gfx.drawimage(0, 0, "constellation");
+		
+		Text.align(Text.CENTER);
+		Text.size = 2;
+		Text.display(Gfx.screenwidthmid, 4, "FILTER EXAMPLE - PRESS 1 - 4 to change");
+		Text.display(Gfx.screenwidthmid, Gfx.screenheight - 20, "Current Filter: " + currentfilter);
+		
+		if (Input.justpressed(Key.ONE)){
+			//Turn on bloom
+			Filter.reset();
+			Filter.bloom = 1;
+			currentfilter = "Bloom = 1.0";
+		}else if (Input.justpressed(Key.TWO)){
+			//Turn on bloom
+			Filter.reset();
+			Filter.bloom = 2;
+			currentfilter = "Bloom = 2.0";
+		}else if (Input.justpressed(Key.THREE)){
+			//Turn on blur
+			Filter.reset();
+			Filter.blur = true;
+			currentfilter = "Blur";
+		}else if (Input.justpressed(Key.FOUR)){
+			//Turn off everything
+			Filter.reset();
+			currentfilter = "None";
+		}
+	}
+}
