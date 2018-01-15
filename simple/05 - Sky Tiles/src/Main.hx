5035b4698b8e31d06e6ed40a3ecebe978fcbb48a
TerryCavanagh <terrygcavanagh@gmail.com>
2018-01-15 18:59:58 +0000

    Migrated 05 - Sky Tiles to haxegon 0.11.0

--- simple/05 - Sky Tiles/src/Main.hx
+++ simple/05 - Sky Tiles/src/Main.hx
@@ -2,6 +2,9 @@ import haxegon.*;
 
 class Main {
 	function new() {
+		//Use the entire window, with a height of 480
+		Gfx.resizescreen(0, 480);
+		
 		//Load in data/graphics/sky.jpg, a split it into 20x20 tiles
 		Gfx.loadtiles("sky", 20, 20);
 		Gfx.linethickness = 25;
@@ -10,8 +13,10 @@ class Main {
 	function update() {
 		//Loop over the screen and draw each tile offset 
 		//from what it should be as time passes
-		for (j in 0 ... 24) {
-			for (i in 0 ... 39) {
+		var width:Int = Convert.toint(Gfx.screenwidth / 20) + 1;
+		var height:Int = Convert.toint(Gfx.screenheight / 20) + 1;
+		for (j in 0 ... height) {
+			for (i in 0 ... width) {
 				Gfx.drawtile(i * 20, j * 20, "sky", Std.int(i + (j * (38 + Core.time))) % Gfx.numberoftiles("sky"));
 			}
 		}
