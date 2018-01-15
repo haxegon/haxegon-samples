9a733365cf0b95508ef351cee0065c0b036cafca
TerryCavanagh <terrygcavanagh@gmail.com>
2018-01-15 20:45:55 +0000

    Migrated 08 - Truetype Fonts to haxegon 0.11.0

--- simple/08 - Truetype Fonts/src/Main.hx
+++ simple/08 - Truetype Fonts/src/Main.hx
@@ -7,41 +7,51 @@ class Main {
 	// or Bitmap fonts (which are crisp and good for low resolutions). This demo
 	// shows truetype fonts only.
 	
-	// For most target platforms, you only need a TTF file, but for HTML5, you need webfont
-	// formats like .eof and .woff. You can create these using a generator like this:
-	// http://www.fontsquirrel.com/tools/webfont-generator	
+	var fontscale:Float;
+	
+	function init(){
+		//Truetype fonts look a LOT better when we don't scale the canvas!
+		Gfx.resizescreen(0, 0);
+	}
+	
 	function update() {
+		//Let's figure out a scale that's proportional to a 480 pixel high screen
+		fontscale = Gfx.screenheight / 480;
+		
 		// Draw a white background
 		Gfx.clearscreen(Col.WHITE);
 		
-		//When changing font, it's usually best to change both the fontface and the size!
 		Text.font = "oswald";
-		Text.size = 32;
-		Text.display(Text.CENTER, 5, "Haxegon TrueType Font examples:", Col.BLACK);
-		Text.display(10, 80, "Oswald", Col.BLACK);
+		Text.size = 32 * fontscale;
+		Text.display(Text.CENTER, 5 * fontscale, "Haxegon TrueType Font examples:", Col.BLACK);
+		Text.size = 16 * fontscale;
+		Text.display(Text.CENTER, 55 * fontscale, "(try resizing this window!)", Col.BLACK);
 		
-		Text.size = 16;
-		Text.display(10, 120, "The quick brown fox jumps over the lazy dog.", Col.BLACK);
+		Text.size = 32 * fontscale;
+		Text.display(10, 80 * fontscale, "Oswald", Col.BLACK);
+		
+		Text.size = 16 * fontscale;
+		Text.display(10, 120 * fontscale, "The quick brown fox jumps over the lazy dog.", Col.BLACK);
 		
 		Text.font = "inconsolata";
-		Text.size = 32;
-		Text.display(10, 180, "Inconsolata", Col.BLACK);
+		Text.size = 32 * fontscale;
+		Text.display(10, 180 * fontscale, "Inconsolata", Col.BLACK);
 		
-		Text.size = 16;
-		Text.display(10, 220, "Amazingly few discotheques provide jukeboxes.", Col.BLACK);
+		Text.size = 16 * fontscale;
+		Text.display(10, 220 * fontscale, "Amazingly few discotheques provide jukeboxes.", Col.BLACK);
 		
 		Text.font = "inconsolata_bold";
-		Text.size = 32;
-		Text.display(10, 280, "Inconsolata Bold", Col.BLACK);
+		Text.size = 32 * fontscale;
+		Text.display(10, 280 * fontscale, "Inconsolata Bold", Col.BLACK);
 		
-		Text.size = 16;	
-		Text.display(10, 320, "Heavy boxes perform quick waltzes and jigs.", Col.BLACK);
+		Text.size = 16 * fontscale;	
+		Text.display(10, 320 * fontscale, "Heavy boxes perform quick waltzes and jigs.", Col.BLACK);
 		
 		Text.font = "shadowsintolight";
-		Text.size = 32;
-		Text.display(10, 380, "Shadows Into Light", Col.BLACK);
+		Text.size = 32 * fontscale;
+		Text.display(10, 380 * fontscale, "Shadows Into Light", Col.BLACK);
 		
-		Text.size = 16;
-		Text.display(10, 420, "Jackdaws love my big sphinx of quartz.", Col.BLACK);
+		Text.size = 16 * fontscale;
+		Text.display(10, 420 * fontscale, "Jackdaws love my big sphinx of quartz.", Col.BLACK);
 	}
 }
